b8e74b5bf2838d5fd6b9ef4cd4ee20ae87044b5c
anggito-m <anggitomuhammadamien@mail.ugm.ac.id>
2024-09-02 13:40:39 +0700

    PatchUP Project

---
+++ PatchUP/ClassPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class person
+{
+   public string Name;
+   public string Email;
+   public string Password;
+
+    public person(string name, string email, string password)
+    {
+
+    }
+    
+}
---
+++ person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class person
+{
+    public string Name;
+    public string Email;
+    public string Password;
+
+    public person(string name, string email, string password)
+	{
+		
+	}
+}
