ce308243b2d500cf3202c5c7ee95332207e5cf2b
anggito-m <anggitomuhammadamien@mail.ugm.ac.id>
2024-09-02 14:19:55 +0700

    add person class

--- PatchUP/ClassPerson.cs
+++ PatchUP/ClassPerson.cs
@@ -8,7 +8,43 @@ public class person
 
     public person(string name, string email, string password)
     {
+        this.Name = name;
+        this.Email = email;
+        this.Password = password;
+    }
+}
+
+public class admin: person
+{
+    public int adminID;
+    public admin(int AdminID, string name, string email, string password ):base(name, email, password)
+    {
+        this.adminID = AdminID;
+    }
+
+    public int PostTutorial()
+    {
+        Console.WriteLine("Apakah ingin melakukan post artikel dan video, 1= Ya, 0=Tidak");
+        int pilihan = Console.Read();
+        return pilihan;
+        //pengembangan selanjutnya menggunakan database
+    }
+    public int DeleteTutorial()
+    {
+        Console.WriteLine("Apakah ingin melakukan delete artikel dan video ini");
+        int pilihan = Console.Read();
+        return pilihan;
+        // pengembangan selanjutnya menggunakan database
 
     }
-    
+}
+
+public class user: person
+{
+    public int userID;
+    public user(int UserID, string name, string email, string password) : base(name, email, password)
+    {
+        this.userID = UserID;
+    }
+
 }
--- PatchUP/Form1.cs
+++ PatchUP/Form1.cs
@@ -6,5 +6,15 @@ namespace PatchUP
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
